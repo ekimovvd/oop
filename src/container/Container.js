0bdea4627ad2cb7fe32ba8dc9f54de75be45a1f3
Екимов Вадим <ekimov_vd@mail.ru>
2021-05-25 16:14:56 +0700

    Сортировка

--- src/container/Container.js
+++ src/container/Container.js
@@ -1,6 +1,14 @@
+import { numberLetters, compare } from "../functions/functions";
+
 export class Container {
   constructor() {
     this.arr = [];
+  }
+
+  sorting() {
+    this.arr.sort((a, b) =>
+      compare(numberLetters(a.name), numberLetters(b.name))
+    );
   }
 
   clear() {
